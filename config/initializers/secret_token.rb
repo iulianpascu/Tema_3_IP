69faf1336a256eeff35f4f6e7864ca98f0dea404
Pascu Iulian <pascu.iulian@gmail.com>
2013-03-26 03:46:33 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Varza::Application.config.secret_token = '4f576b3e2300f4b28a62b256450c6f0d74660693a07ec8a66ed9932c2dad9b7cf67ab3ea0f9d6dde6f5b6c4068f19d7e044add02d3906a51ef3cbbd51c56e6db'
