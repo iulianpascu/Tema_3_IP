b8dfab6b1155fe999f5d12dc7380993a8568e972
Octavian <octavian@octavian-VirtualBox.(none)>
2013-04-05 00:41:40 +0300

    coloane curs rezolvate

--- db/migrate/20130403211740_add_an_to_curs.rb
+++
@@ -1,5 +0,0 @@
-class AddAnToCurs < ActiveRecord::Migration
-  def change
-    add_column :cursuri, :an, :integer
-  end
-end
--- db/migrate/20130403222920_remove_an_from_curs.rb
+++
@@ -1,9 +0,0 @@
-class RemoveAnFromCurs < ActiveRecord::Migration
-  def up
-    remove_column :cursuri, :an
-  end
-
-  def down
-    add_column :cursuri, :an, :integer
-  end
-end
