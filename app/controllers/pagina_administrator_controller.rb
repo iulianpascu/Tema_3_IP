9da7be4a19585aad1353b59355889b967f949b99
Iulian Pascu <pascu.iulian@gmail.com>
2013-04-09 22:52:40 +0300

    merge fix

--- app/controllers/pagina_administrator_controller.rb
+++ app/controllers/pagina_administrator_controller.rb
@@ -1,21 +1,6 @@
 class PaginaAdministratorController < ApplicationController
 
 	def pagAdmin
-		cursuri = Curs.all
-
-		@valori_admin =Array.new
-			cursuri.each do |c|
-				evaluare_disponibila = EvaluareDisponibila.find_by_curs_id(c.id)
-				evaluare_completata =
-				EvaluareCompletata.find(:all, :conditions =>
-				{:evaluare_disponibila_id => evaluare_disponibila.id })
-					if evaluare_completata
-						@valori_admin << c.an
-						@valori_admin << c.nume
-						profesor = Profesor.find_by_id(c.profesor_id)
-						@valori_admin << profesor.nume
-					end
-			end
-
+		@cursuri = Curs.all
 	end
 end
